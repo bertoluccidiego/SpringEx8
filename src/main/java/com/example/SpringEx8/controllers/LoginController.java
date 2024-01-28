d36f1e72a215f766ee72eb1e2797c4ffcb543299
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2024-01-28 15:57:23 -0300

    # Creating a login webapp
    * Login page and controller created

---
+++ src/main/java/com/example/SpringEx8/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.SpringEx8.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String loginGet() {
+        return "login.html";
+    }
+}
